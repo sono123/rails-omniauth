8a0897405f1e65b4bd88ae35a94150c4a513c53f
steveono <steveono@gmail.com>
2016-05-23 23:39:10 -0600

    Add signin and signout to nav.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   	end
 
   	def user_signed_in?
-  		return true if  current_user
+  		return true if current_user
   	end
 
   	def correct_user?
