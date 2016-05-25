f3e418669ce0d0b52f356b99e0626b285d3d69b1
steveono <steveono@gmail.com>
2016-05-24 22:13:42 -0600

    Bug fixing.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,11 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["onmiauth.auth"]
 
-    user = User.where(:provider => auth['provider'],
-    									:uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    user = User.where(:provider => auth['provider'], :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
     debugger
-    
+
     redirect_to root_url, :notice => 'Signed In!'
   end
 
